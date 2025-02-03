14d3f7a1a7e079e9b7c72f5a43098876669f70a4
Kyrylo Katkov <kyrylo.katkov@gmail.com>
2025-02-03 15:21:22 +0200

    added one more dev

--- src/mock/developersInfo.ts
+++ src/mock/developersInfo.ts
@@ -1,5 +1,10 @@
 import { TDevelopersInfo } from "../types/TDevelopersInfo";
 import { dariaCherkasovaDevInfo } from "./developers/daria-cherkasova";
 import { kirillKatkovDevInfo } from "./developers/kirill-katkov";
+import { data as dimaDomnichInfo } from "./developers/dima-domnich";
 
-export const developersInfo: TDevelopersInfo = [dariaCherkasovaDevInfo, kirillKatkovDevInfo];
+export const developersInfo: TDevelopersInfo = [
+  dariaCherkasovaDevInfo,
+  kirillKatkovDevInfo,
+  dimaDomnichInfo
+];
